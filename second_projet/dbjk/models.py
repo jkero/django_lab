0ab912b9d2e56dde8bd8c6c428c6666664889531
jkeroack <jk@spherik.ca>
2023-11-15 08:04:16 -0500

    avant mig

--- second_projet/dbjk/models.py
+++ second_projet/dbjk/models.py
@@ -6,13 +6,10 @@ from django.db import models
 
 class Plante(models.Model):
     plante_texte = models.CharField(max_length=200)
-    plante_ec_min = value = models.DecimalField(max_digits=2, decimal_places=2)
-    plante_ec_max = value = models.DecimalField(max_digits=2, decimal_places=2)
-    plante_ph_min = value = models.DecimalField(max_digits=2, decimal_places=2)
-    plante_ph_max = value = models.DecimalField(max_digits=2, decimal_places=2)
+    plante_ec_min = models.DecimalField(max_digits=2, decimal_places=2)
+    plante_ec_max = models.DecimalField(max_digits=2, decimal_places=2)
+    plante_ph_min = models.DecimalField(max_digits=2, decimal_places=2)
+    plante_ph_max = models.DecimalField(max_digits=2, decimal_places=2)
+    plante_prod = models.CharField(max_length=200)
 
 
-class PlanteVar(models.Model):
-    planteprinc = models.ForeignKey(Plante, on_delete=models.CASCADE)
-    plantevar_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
